abe1b8b24f27fe765d452b7c93085c770f70b21c
Ozelot Vanilla <shuihenyun@qq.com>
2021-03-03 13:24:09 +0800

    updater worked

--- Jathon/updater.py
+++ Jathon/updater.py
@@ -1,1 +1,4 @@
-import urllib as u
+import urllib.request as u
+
+u.urlretrieve("https://raw.githubusercontent.com/OzelotVanilla/aTool/main/Jathon/lib/builtin.java", "builtin.java")
+u.urlretrieve("https://raw.githubusercontent.com/OzelotVanilla/aTool/main/Jathon/lib/IntList.java", "IntList.java")
